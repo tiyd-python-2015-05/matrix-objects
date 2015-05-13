de87b52416ff2766adb3b0bf80a7f211ddd8e7b1
John Waldrep <johnwaldrep@gmail.com>
2015-05-13 13:33:13 -0400

    Add Matrix and Vector classes; 2/2 tests pass

---
+++ matrix_math.py
@@ -0,0 +1,8 @@
+class Matrix():
+    def __init__(self, my_list):
+        pass
+
+
+class Vector():
+    def __init__(self, my_vector):
+        pass
---
+++ test_matrix_math.py
@@ -0,0 +1,6 @@
+from matrix_math import *
+def test_matrix_class():
+    m = Matrix([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+
+def test_vector_class():
+    v = Vector([1, 5])
