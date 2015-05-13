833eaf66d3d0ca791e9432f9d2f2170e80113125
John Waldrep <johnwaldrep@gmail.com>
2015-05-13 14:03:20 -0400

    Add error/shape handling; 7/7 pass

--- matrix_math.py
+++ matrix_math.py
@@ -1,8 +1,25 @@
 class Matrix():
     def __init__(self, my_list):
-        pass
+        self.my_list = my_list
+        self.my_shape = self.shape()
 
+
+    def shape(self):
+        try:
+            return (len(self.my_list), len(self.my_list[0]))
+        except (TypeError,IndexError):
+            raise ValueError('Not a 2d matrix')
 
 class Vector():
-    def __init__(self, my_vector):
-        pass
+    def __init__(self, my_list):
+        self.my_list = my_list
+        self.my_shape = self.shape()
+
+    def shape(self):
+        try:
+            if len(self.my_list[0]) > 1:
+                raise ValueError('Not a 1d vector')
+        except TypeError:
+            return (len(self.my_list),)
+        except:
+            raise ValueError('Not a 1d vector')
--- test_matrix_math.py
+++ test_matrix_math.py
@@ -1,6 +1,34 @@
 from matrix_math import *
+from nose.tools import raises
+
 def test_matrix_class():
     m = Matrix([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
 
 def test_vector_class():
     v = Vector([1, 5])
+
+@raises(ValueError)
+def test_2d_vector_exception():
+    v = Vector([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+
+@raises(ValueError)
+def test_empty_vector_exception():
+    v = Vector([])
+
+@raises(ValueError)
+def test_1d_matrix_exception():
+    m = Matrix([1,5])
+
+@raises(ValueError)
+def test_empty_matrix_exception():
+    v = Matrix([])
+
+def test_matrix_class():
+    v = Vector([1, 5])
+    assert v.shape() == (2,)
+
+def test_matrix_shape():
+    m = Matrix([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+    assert m.shape() == (3,3)
+    m = Matrix([[1,2],[3,4],[5,6]])
+    assert m.shape() == (3,2)
