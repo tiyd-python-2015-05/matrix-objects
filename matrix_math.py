b713340aba916845c9c7e3785d4843e7aca0582b
John Waldrep <johnwaldrep@gmail.com>
2015-05-13 15:11:11 -0400

    Vector add/sub/scalar multiplication added with lots of exceptions. 13/13 pass

--- matrix_math.py
+++ matrix_math.py
@@ -1,3 +1,37 @@
+class ShapeException(Exception):
+    pass
+
+class Vector():
+    def __init__(self, my_list):
+        self.my_list = my_list
+        self.my_shape = self.shape()
+    def __add__(self, other):
+        if self.shape() != other.shape():
+            raise ShapeException
+        return Vector([self.my_list[i] + other.my_list[i] for i in range(self.shape()[0])])
+
+    def __sub__(self, other):
+        other = other * -1
+        return self + other
+    def __mul__(self, other):
+        if type(other) is int or type(other) is float:
+            return Vector([i * other for i in self.my_list])
+        else:
+            return None
+    def __eq__(self, other):
+        return self.my_list == other.my_list
+
+    def shape(self):
+        try:
+            if len(self.my_list[0]) > 1:
+                raise ValueError('Not a 1d vector')
+        except TypeError:
+            return (len(self.my_list),)
+        except:
+            raise ValueError('Not a 1d vector')
+    def dot(self, other):
+        pass
+        
 class Matrix():
     def __init__(self, my_list):
         self.my_list = my_list
@@ -9,17 +43,3 @@ class Matrix():
             return (len(self.my_list), len(self.my_list[0]))
         except (TypeError,IndexError):
             raise ValueError('Not a 2d matrix')
-
-class Vector():
-    def __init__(self, my_list):
-        self.my_list = my_list
-        self.my_shape = self.shape()
-
-    def shape(self):
-        try:
-            if len(self.my_list[0]) > 1:
-                raise ValueError('Not a 1d vector')
-        except TypeError:
-            return (len(self.my_list),)
-        except:
-            raise ValueError('Not a 1d vector')
--- test_matrix_math.py
+++ test_matrix_math.py
@@ -23,12 +23,77 @@ def test_1d_matrix_exception():
 def test_empty_matrix_exception():
     v = Matrix([])
 
-def test_matrix_class():
-    v = Vector([1, 5])
-    assert v.shape() == (2,)
+
+m = Vector([3, 4])
+n = Vector([5, 0])
+
+v = Vector([1, 3, 0])
+w = Vector([0, 2, 4])
+u = Vector([1, 1, 1])
+y = Vector([10, 20, 30])
+z = Vector([0, 0, 0])
+
+A = Matrix([[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]])
+B = Matrix([[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]])
+C = Matrix([[1, 2],
+     [2, 1],
+     [1, 2]])
+D = Matrix([[1, 2, 3],
+     [3, 2, 1]])
+
+def test_matrix_class_and_shape():
+    assert m.shape() == (2,)
+    assert v.shape() == (3,)
 
 def test_matrix_shape():
     m = Matrix([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
     assert m.shape() == (3,3)
     m = Matrix([[1,2],[3,4],[5,6]])
     assert m.shape() == (3,2)
+    assert A.shape() == (3, 3)
+    assert C.shape() == (3, 2)
+    assert D.shape() == (2, 3)
+def test_vector_eq():
+    p = Vector([3,4])
+    assert m == p
+    
+def test_vector_add():
+    """
+    [a b]  + [c d]  = [a+c b+d]
+
+    Matrix + Matrix = Matrix
+    """
+    assert v + w == Vector([1, 5, 4])
+    assert u + y == Vector([11, 21, 31])
+    assert u + z == u
+
+def test_vector_add_is_communicative():
+    assert w + y == y + w
+
+def test_vector_sub():
+    """
+    [a b]  - [c d]  = [a-c b-d]
+
+    Matrix + Matrix = Matrix
+    """
+    assert v - w == Vector([1, 1, -4])
+    assert w - v == Vector([-1, -1, 4])
+    assert y - z == y
+    assert w - u == (z - (u - w))
+
+
+@raises(ShapeException)
+def test_vector_sub_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    m - v
+
+"""
+def test_matrix_add():
+    assert False
+def test_matrix_sub():
+    assert False
+"""
